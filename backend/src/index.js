c9af48d5d0a52fdad214ea447333dc40fd9777cd
Zal-n <katonazalan816@gmail.com>
2025-12-03 19:08:16 +0100

    docker setup 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
-import e from 'express';
-import colorLog, { errorLog } from './utils/logger.js';
+import express from 'express';
+import { errorLog, colorLog } from './utils/logger.js';
 import authRouter from './route/authRoutes.js';
+import { config } from './config/env.js';
 
-const app = Express();
+const app = express();
+const PORT = config.PORT;
+
+
 app.use(colorLog);
 
 app.use('/auth', authRouter);
 
 
 app.get('/healthcheck', (req,res)=>res.status(200).json('Ok'));
+
 app.use(errorLog);
 
 app.listen(PORT, ()=>{
