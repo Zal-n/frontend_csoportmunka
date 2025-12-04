4667532cc2a0f32575088ac7ed182f406fc3978c
Katona Zalán <katonazalan2453@PETOFI-ASZOD.LAN>
2025-12-04 12:48:15 +0100

    log debug

--- backend/src/route/authRoutes.js
+++ backend/src/route/authRoutes.js
@@ -5,6 +5,6 @@ import {validateFieldCount, validateRequiredFields} from 'psgutil';
 const authRouter = Router();
 
 authRouter.post('/register', validateFieldCount(3), validateRequiredFields(['username', 'password', 'email']), Register);
-authRouter.post('/login',validateFieldCount(2), validateRequiredFields(['credential', 'password']), Login);
+authRouter.post('/login', validateFieldCount(2), validateRequiredFields(['credential', 'password']), Login);
 
 export default authRouter;
