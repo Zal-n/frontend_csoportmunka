d3c7345abfcbc69f1263fc9975caf1bc7641cc62
Zal-n <katonazalan816@gmail.com>
2025-12-04 17:13:13 +0100

    folder rename

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { Login, Logout, Refresh, Register } from '../controller/authController.js';
+import { Login, Logout, Refresh, Register } from '../controllers/authController.js';
 import { validateFieldCount, validateRequiredFields } from 'psgutil';
 
 const authRouter = Router();
