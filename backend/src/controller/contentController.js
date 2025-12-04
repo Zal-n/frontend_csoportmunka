1da2980e1e738afdb06d0b3e9446bf1705fddf9e
Zal-n <katonazalan816@gmail.com>
2025-12-04 17:54:16 +0100

    Refactor content routes and controller: add missing imports, implement recipe retrieval by category, id, and name, and enhance recipe upload functionality with transaction handling.

--- backend/src/controller/contentController.js
+++ backend/src/controller/contentController.js
@@ -1,35 +1,45 @@
+import { pool } from '../config/mysql.js';
 
-export function GetRecipesByCategory(req, res, next){
-  try{
+export function GetRecipesByCategory(req, res, next) {
+  try {
     const category = req.query.category;
 
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 }
-export function GetRecipesById(req, res, next){
-  try{
+export function GetRecipesById(req, res, next) {
+  try {
     const id = req.query.id;
 
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 }
-export function GetRecipesByName(req, res, next){
-  try{
+export function GetRecipesByName(req, res, next) {
+  try {
     const name = req.query.name;
 
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 }
-export function UploadRecipe(req, res, next){
-  try{
-    const {name, ingredients, descripiton, instructions} = req.body;
+export async function UploadRecipe(req, res, next) {
+  const conn = await pool.getConnection();
+  try {
+    const { name, description, ingredients, instructions } = req.body;
+    await conn.beginTransaction();
+    const [result] = await conn.query('INSERT INTO recipes (name, description, ingredients, instructions, uploader_id) VALUES (?, ?, ?, ?, ?);', [name, description, JSON.stringify(ingredients), JSON.stringify(instructions), req.user.id]);
 
-    
+    await conn.query('SELECT * FROM recipes WHERE id = ?;', [result.insertId]);
 
-  }catch(error){
+    await conn.commit();
+
+    return res.status(200).json({ message: 'Uploaded successfully!' });
+  } catch (error) {
+    await conn.rollback();
     next(error);
+  } finally {
+    conn.release();
   }
 }
--- backend/src/routes/contentRoutes.js
+++ backend/src/routes/contentRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import authenticateToken from '../utils/auth.js'
+import {authenticateToken} from '../utils/auth.js'
+import { validateFieldCount, validateRequiredFields } from 'psgutil';
+import { GetRecipesByCategory, GetRecipesById, GetRecipesByName, UploadRecipe } from '../controller/contentController.js';
 
 const contentRouter = Router();
 
-contentRouter.get('/category/:category');
-contentRouter.get('/id/:id');
-contentRouter.get('/name/:name');
-contentRouter.post('/upload', authenticateToken);
+contentRouter.get('/category/:category', GetRecipesByCategory);
+contentRouter.get('/id/:id', GetRecipesById);
+contentRouter.get('/name/:name', GetRecipesByName);
+contentRouter.post('/upload', validateFieldCount(4), validateRequiredFields(['name', 'ingredients', 'description', 'instructions']), authenticateToken, UploadRecipe);
 
 
 export default contentRouter;
