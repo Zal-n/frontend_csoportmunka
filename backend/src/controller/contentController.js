3bbc59da0b0841d791a241e1778181df112938df
Zal-n <katonazalan816@gmail.com>
2025-12-04 18:20:27 +0100

    Refactor recipe retrieval functions to use route parameters and enhance error handling; update upload route validation to include category.

--- backend/src/controller/contentController.js
+++ backend/src/controller/contentController.js
@@ -1,41 +1,88 @@
 import { pool } from '../config/mysql.js';
 
-export function GetRecipesByCategory(req, res, next) {
+export async function GetRecipesByCategory(req, res, next) {
   try {
-    const category = req.query.category;
+    const category = req.params.category;
+
+    const [result] = await pool.query('SELECT * FROM recipes WHERE category = ?;', [category]);
+
+    if (result.length < 1) return res.status(404).json({ message: 'Category not found!' });
+
+    const resultObject = {
+      id: result[0].id,
+      name: result[0].name,
+      category: result[0].category,
+      description: result[0].description,
+      ingredients: JSON.parse(result[0].ingredients),
+      instructions: JSON.parse(result[0].instructions),
+    }
+
+    return res.status(200).json(resultObject);
 
   } catch (error) {
     next(error);
   }
 }
-export function GetRecipesById(req, res, next) {
+
+export async function GetRecipesById(req, res, next) {
   try {
-    const id = req.query.id;
+    const id = req.params.id;
+
+    const [result] = await pool.query('SELECT * FROM recipes WHERE id = ?;', [id]);
+
+    if (result.length < 1) return res.status(404).json({ message: 'Category not found!' });
+
+    const resultObject = {
+      id: result[0].id,
+      name: result[0].name,
+      category: result[0].category,
+      description: result[0].description,
+      ingredients: JSON.parse(result[0].ingredients),
+      instructions: JSON.parse(result[0].instructions),
+    }
+    return res.status(200).json(resultObject);
 
   } catch (error) {
     next(error);
   }
 }
-export function GetRecipesByName(req, res, next) {
-  try {
-    const name = req.query.name;
 
+export async function GetRecipesByName(req, res, next) {
+  try {
+    const name = req.params.name;
+
+    const [result] = await pool.query('SELECT * FROM recipes WHERE name LIKE ?;', [`%${name}%`]);
+
+    if (result.length < 1) return res.status(404).json({ message: 'Category not found!' });
+
+    const resultObject = {
+      id: result[0].id,
+      name: result[0].name,
+      category: result[0].category,
+      description: result[0].description,
+      ingredients: JSON.parse(result[0].ingredients),
+      instructions: JSON.parse(result[0].instructions),
+    }
+    return res.status(200).json(resultObject);
   } catch (error) {
     next(error);
   }
 }
+
 export async function UploadRecipe(req, res, next) {
   const conn = await pool.getConnection();
   try {
+
     const { name, description, ingredients, instructions } = req.body;
+
     await conn.beginTransaction();
     const [result] = await conn.query('INSERT INTO recipes (name, description, ingredients, instructions, uploader_id) VALUES (?, ?, ?, ?, ?);', [name, description, JSON.stringify(ingredients), JSON.stringify(instructions), req.user.id]);
 
     await conn.query('SELECT * FROM recipes WHERE id = ?;', [result.insertId]);
-
     await conn.commit();
 
     return res.status(200).json({ message: 'Uploaded successfully!' });
+
   } catch (error) {
     await conn.rollback();
     next(error);
