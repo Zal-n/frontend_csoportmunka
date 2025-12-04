1ac54c01910e06f43dcf0b053acaf2508742e1a5
Zal-n <katonazalan816@gmail.com>
2025-12-04 16:18:20 +0100

    token refresh controller and route added

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,6 @@
 import { pool } from "../config/mysql.js";
 import argon2 from 'argon2';
-import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js";
+import { generateAccessToken, generateRefreshToken, verifyRefreshToken } from "../utils/jwt.js";
 import validate from "psgutil";
 
 
@@ -34,6 +34,7 @@ export async function Register(req, res, next) {
     conn.release();
   }
 }
+
 export async function Login(req, res, next) {
   const { credential, password } = req.body;
   try {
@@ -82,6 +83,33 @@ export async function Login(req, res, next) {
     next(error);
   }
 }
+
 export async function Refresh(req, res, next) {
-  
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.status(401).json({ message: 'Refresh Token required.' });
+
+    const decoded = verifyRefreshToken(refreshToken);
+    if (!decoded) return res.status(401).json({ message: 'Invalid refresh token.' });
+
+    const payload = {
+      username: decoded.username,
+      id: decoded.id,
+      rights: decoded.rights,
+    }
+
+    const accessToken = generateAccessToken(payload);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 15 * 60 * 1000
+    })
+
+    return res.status(200).json({ message: 'Token refreshed.' });
+
+  } catch (err) {
+    next(err);
+  }
 }
--- backend/src/route/authRoutes.js
+++ backend/src/route/authRoutes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { Login, Register } from '../controller/authController.js';
+import { Login, Refresh, Register } from '../controller/authController.js';
 import {validateFieldCount, validateRequiredFields} from 'psgutil';
 
 const authRouter = Router();
 
 authRouter.post('/register', validateFieldCount(3), validateRequiredFields(['username', 'password', 'email']), Register);
 authRouter.post('/login', validateFieldCount(2), validateRequiredFields(['credential', 'password']), Login);
+authRouter.post('/refresh', Refresh);
 
 export default authRouter;
