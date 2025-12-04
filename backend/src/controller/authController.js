9e7808fa726afbf54d0eb313454c9d98d258cfcb
Katona Zalán <katonazalan2453@PETOFI-ASZOD.LAN>
2025-12-04 11:45:19 +0100

    cors added

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,16 +1,22 @@
 
 export async function Register(req, res, next) {
   try {
-
     return res.status(200).json({ message: 'Sikeres regisztráció'});
+
   } catch (error) {
     next(error);
   }
 }
 export function Login(req, res, next) {
+  const {username, password} = req.body;
   try {
+    
+
+
+
 
     return res.status(200).json({ message: 'Sikeres bejelentkezés', username: 'admin', email: 'test@test.com'});
+
   } catch (error) {
     next(error);
   }
